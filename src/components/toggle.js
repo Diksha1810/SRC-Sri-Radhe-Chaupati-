f3a14c94489973acd671df9f6c4c5c3ab2f24d82
Diksha1810 <dikshaabbi18@gmail.com>
2023-12-01 14:49:21 +0530

    changes

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,1 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react"
+import { FaMoon } from "react-icons/fa"
+function Toggle() {
+  const [theme, setTheme] = useState("light-theme")
+  const toggleTheme = () => {
+    if (theme =="dark-theme") {
+      setTheme('light-theme');
+      console.log("hello"); 
+
+    }
+    else {
+      setTheme("dark-theme")
+    }
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+
+
+  }, [theme]);
+
+  return (
+    <>
+      <div className="container-fluid">
+<div className="toggle-theme">   
+
+
+ <button type="button" className="btn border btn-light "onClick={toggleTheme}><FaMoon size="3em" className="text-danger"/></button>
+    
+</div>
+
+     
+
+      </div>
+    </>
+  )
+}
+export default Toggle 
